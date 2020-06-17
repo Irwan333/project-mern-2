26ca5bc9777ce5b51ee93976b969bc921fbcb3a6
Azel333 <azelazero@gmail.com>
2020-06-17 17:43:21 +0700

    perubahan 1

---
+++ routes/post.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const {
+  getPosts,
+  createPost,
+  postsByUser,
+  postById,
+  isPoster,
+  updatePost,
+  deletePost,
+} = require("../controllers/post");
+const {
+  requireSignin
+} = require("../controllers/auth");
+const {
+  userById
+} = require("../controllers/user");
+const {
+  runValidation
+} = require("../validator");
+const {
+  createPostValidator
+} = require("../validator/post");
+const router = express.Router();
+
+router.get("/posts", getPosts);
+router.post(
+  "/post/new/:userId",
+  requireSignin,
+  createPost,
+  createPostValidator,
+  runValidation
+);
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+
+router.param("userId", userById);
+router.param("postId", postById);
+
+module.exports = router;
